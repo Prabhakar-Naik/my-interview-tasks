384854e17068c04af8ef2d08a4c9205b39d46112
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2025-02-26 14:56:46 +0530

    project and task completed

---
+++ beetexting-task/src/test/java/com/spring/boot/beetextingtask/BeetextingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.beetextingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeetextingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
