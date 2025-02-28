b47d4d5973be103add912d354963608bbd4ae53c
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2025-02-28 11:49:45 +0530

    trade one project setup

---
+++ trade-one/src/test/java/com/trade/one/TradeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
